dcf4cc5045e1c55d527ef241b85c4d166ce087f9
Abiola Osinupebi <abiolaosinupebicsa@gmail.com>
2021-01-01 10:20:46 +0000

    odd numbers

---
+++ return.js
@@ -0,0 +1,14 @@
+// return & console.log
+// console.log
+
+/*const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); */
+
+// return
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John'); 
